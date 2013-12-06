4d4964271b410db001f809da6629e3807608d3bb
Smith Ascari <smithascari@gmail.com>
2013-12-06 15:45:59 -0300

    Tela de Cadastro de Cliente

--- src/ufpb/pas2/spun/GerenciarCliente.java
+++ src/ufpb/pas2/spun/GerenciarCliente.java
@@ -2,7 +2,12 @@ package ufpb.pas2.spun;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.Button;
 
 public class GerenciarCliente extends Activity {
 
@@ -10,8 +15,22 @@ public class GerenciarCliente extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_gerenciar_cliente);
+		
+		Button addCliente = (Button)findViewById(R.id.add_cliente);
+		addCliente.setOnClickListener(new LinkNovoClienteActivity());
 	}
-
+	
+	private class LinkNovoClienteActivity implements OnClickListener{
+		
+		@Override
+		public void onClick(View b) {
+			Intent i = new Intent(GerenciarCliente.this, NovoCliente.class);
+			startActivity(i);
+			
+		}
+    }
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
